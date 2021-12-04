ab56c1e99710adf4c4ec421d1e4790ee100117ce
Caio Pereira <caiopereira1998@hotmail.com>
2021-12-04 11:14:50 -0300

    Payment, one-to-one association

--- src/main/java/com/loja/buynow/config/TestConfig.java
+++ src/main/java/com/loja/buynow/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.loja.buynow.entities.Category;
 import com.loja.buynow.entities.Order;
 import com.loja.buynow.entities.OrderItem;
+import com.loja.buynow.entities.Payment;
 import com.loja.buynow.entities.Product;
 import com.loja.buynow.entities.User;
 import com.loja.buynow.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2021-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
